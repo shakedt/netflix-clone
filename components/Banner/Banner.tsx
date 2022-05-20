0020968ec97ccab952fb55df6d3be920c728573d
shaked <dokuroc@gmail.com>
2022-05-20 17:38:02 +0300

    add icons from gogle fonts area

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -1,37 +1,46 @@
 import React from "react";
-import styles from "./Banner.module.css";
+import Image from "next/image";
 
+import styles from "./Banner.module.css";
 interface ibanner {
   title: string;
-  subtitle: string;
+  subTitle: string;
   imgUrl: string;
 }
 
 const Banner = (props: ibanner) => {
-  const { title, subtitle, imgUrl } = props;
+  const { title, subTitle, imgUrl } = props;
 
   const handleClick = () => {};
 
   return (
     <div className={styles.container}>
-      <div className={styles.leftWarpper}>
-        <div className={styles.left}></div>
-        <h3 className={styles.title}>{title}</h3>
-        <h3 className={styles.subtitle}>{subtitle}</h3>
-        <div className={styles.playBtnWrapper}></div>
-        <button className={styles.btnWithIcon} onClick={handleClick}>
-          Play
-        </button>
+      <div className={styles.leftWrapper}>
+        <div className={styles.left}>
+          <div className={styles.nseriesWrapper}>
+            <p className={styles.firstLetter}>N</p>
+            <p className={styles.series}>etflix clone</p>
+          </div>
+          <h3 className={styles.title}>{title}</h3>
+          <h3 className={styles.subTitle}>{subTitle}</h3>
+
+          <div className={styles.playBtnWrapper}>
+            <button className={styles.btnWithIcon} onClick={handleClick}>
+              <Image
+                src="/static/PlayButton.svg"
+                alt="Play icon"
+                width="32px"
+                height="32px"
+              />
+              <span className={styles.playText}>Play</span>
+            </button>
+          </div>
+        </div>
       </div>
       <div
-        className={styles.bannerImage}
+        className={styles.bannerImg}
         style={{
-          backgroundImage: `url(${imgUrl})`,
-          width: "100%",
-          height: "100%",
-          position: "absolute",
-          backgroundSize: "cover",
-          backgroundPosition: "50% 50%",
+          backgroundImage: `url(${imgUrl}`,
         }}
       ></div>
     </div>
